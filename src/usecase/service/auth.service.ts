d2ff986aeeda485516e2d3c1974d6c676c284c94
oberdansoldi <oberdan_soldi@outlook.com.br>
2023-09-06 11:35:04 -0300

    style: remove import

--- src/usecase/service/auth.service.ts
+++ src/usecase/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
